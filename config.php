0a563789ab9981d395dc4295779661fa746165b2
jonkofee <jonkofee@icloud.com>
2017-10-23 13:41:52 +0300

    hook

--- config.php
+++ config.php
@@ -1,3 +1,6 @@
 <?php
 
 $domain = 'https://jonkofee-music-bot-jonkofee.c9users.io/';
+
+$bot_api_key  = '461745599:AAHzddZi6dUJ2o2eOOhvsP1ecgnB8WQF5iM';
+$bot_username = 'jonkofee_music_bot';
--- hook.php
+++ hook.php
@@ -4,18 +4,14 @@ require __DIR__ . '/vendor/autoload.php';
 
 require 'config.php';
 
-$hook_url = $domain . 'hook.php';
-
 try {
 	// Create Telegram API object
 	$telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
-	// Set webhook
-	$result = $telegram->setWebhook($hook_url);
-	if ($result->isOk()) {
-		echo $result->getDescription();
-	}
+	// Handle telegram webhook request
+	$telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
+	// Silence is golden!
 	// log telegram errors
-	 echo $e->getMessage();
+	// echo $e->getMessage();
 }
