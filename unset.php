5066fc47b6b68f0413a5718787017e0d28f3db08
jonkofee <jonkofee@icloud.com>
2017-10-23 16:10:49 +0300

    test

--- unset.php
+++ unset.php
@@ -4,16 +4,15 @@
  * This file is intended to unset the webhook.
  * Uncommented parameters must be filled
  */
-
 // Load composer
 require_once __DIR__ . '/vendor/autoload.php';
-
+use Longman\TelegramBot\Request;
 require 'config.php';
 
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
-
+var_dump(Request::getWebhookInfo());die;
     // Delete webhook
     $result = $telegram->deleteWebhook();
 
