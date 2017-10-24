4055fc1c6c23109b043209ccde69ff96068af8a3
jonkofee <jonkofee@icloud.com>
2017-10-24 10:23:24 +0300

    add info

---
+++ info.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * README
+ * This file is intended to unset the webhook.
+ * Uncommented parameters must be filled
+ */
+// Load composer
+require_once __DIR__ . '/vendor/autoload.php';
+use Longman\TelegramBot\Request;
+require 'config.php';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+	var_dump(Request::getWebhookInfo());die;
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    echo $e->getMessage();
+}
