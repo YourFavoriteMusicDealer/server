c5a423b145b6d5b179c530e0131c984a995a557e
jonkofee <jonkofee@icloud.com>
2017-10-24 11:14:43 +0300

    test

--- hook.php
+++ hook.php
@@ -84,11 +84,8 @@ try {
 	$switch_element = mt_rand(0, 9) < 5 ? 'true' : 'false';
 
 	$inline_keyboard = new InlineKeyboard([
-		['text' => 'inline', 'switch_inline_query' => $switch_element],
-		['text' => 'inline current chat', 'switch_inline_query_current_chat' => $switch_element],
-	], [
+		['text' => 'inline'],
 		['text' => 'callback', 'callback_data' => 'identifier'],
-		['text' => 'open url', 'url' => 'https://github.com/php-telegram-bot/core'],
 	]);
 
 	$data = [
