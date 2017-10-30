b80a680160a3c43f5e1b9d6cba250ce02483c49b
jonkofee <jonkofee@icloud.com>
2017-10-30 23:28:15 +0300

    test

--- Commands/CallbackqueryCommand.php
+++ Commands/CallbackqueryCommand.php
@@ -71,7 +71,7 @@ class CallbackqueryCommand extends SystemCommand
 
         $data = [
             'callback_query_id' => $callback_query_id,
-            'text'              => serialize($callback_query),
+            'text'              => serialize($callback_query->getInlineMessageId()),
             'show_alert'        => true,
         ];
 
