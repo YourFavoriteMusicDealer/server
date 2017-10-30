18516cdf6dfd8da6b09d7bfa881a450c59e84f95
jonkofee <jonkofee@icloud.com>
2017-10-30 23:16:23 +0300

    json

--- Commands/CallbackqueryCommand.php
+++ Commands/CallbackqueryCommand.php
@@ -71,7 +71,7 @@ class CallbackqueryCommand extends SystemCommand
 
         $data = [
             'callback_query_id' => $callback_query_id,
-            'text'              => var_dump($callback_query),
+            'text'              => json_encode($callback_query),
             'show_alert'        => true,
         ];
 
