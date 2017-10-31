acfbb09548af917f8e26db862871bcda4b32523c
jonkofee <jonkofee@icloud.com>
2017-10-31 14:33:08 +0300

    new url

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$domain = 'https://jk-music-bot.herokuapp.com/';
+$domain = 'https://dymchenko-art.ru/';
 
 $bot_api_key  = '461745599:AAHzddZi6dUJ2o2eOOhvsP1ecgnB8WQF5iM';
 $bot_username = 'jonkofee_music_bot';
