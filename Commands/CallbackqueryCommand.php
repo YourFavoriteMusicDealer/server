6c987a158f4540c73f0ce02d202640c2d9f8400b
Prischenko Sergey <jonkofee@icloud.com>
2018-01-02 18:39:56 +0300

    change alert text

--- Commands/CallbackqueryCommand.php
+++ Commands/CallbackqueryCommand.php
@@ -148,7 +148,7 @@ class CallbackqueryCommand extends SystemCommand
 
 	    $data = [
 		    'callback_query_id' => $callback_query_id,
-		    'text'              => 'Хорошо, я запомнил😉',
+		    'text'              => 'Понял, принял😉',
 		    'show_alert'        => false,
 	    ];
 
