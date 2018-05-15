7605ac27dd5f12d8777c3cc620191bc44a3cfa80
Prischenko Sergey <jonkofee@icloud.com>
2018-05-15 18:33:17 +0300

    :zap: audio url

--- api/controllers/SongController.php
+++ api/controllers/SongController.php
@@ -13,15 +13,39 @@ class SongController extends Controller
 	{
 		$track = Track::findFirst($id);
 
+		if (!$track) throw new Exception('Такая песня отсутствует', 404);
+
 		$arrTrack = $track->toArray([
 			'artist',
 			'title',
 			'img'
 		]);
 
-		$arrTrack['audio'] = $track->getAudioUrl();
+		$arrTrack['audio'] = "http://{$this->request->getServerName()}:{$this->request->getPort()}/song/{$id}/file";
 
 		return $arrTrack;
+	}
+
+	/**
+	 * @Get('/{id}/file')
+	 */
+	public function getFileAction($id)
+	{
+		$track = Track::findFirst($id);
+
+		if (!$track) throw new Exception('Такая песня отсутствует', 404);
+
+		$telegramUrl = $track->getAudioUrl();
+
+		if ($fp_remote = fopen($telegramUrl, 'rb')) {
+			header("Content-Type: audio/mpeg");
+
+			fpassthru($fp_remote);
+			fclose($fp_remote);
+			exit;
+		} else {
+			throw new Exception('Не получается загрузить файл');
+		}
 	}
 
 }
